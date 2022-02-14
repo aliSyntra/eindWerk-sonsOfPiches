b18e24b70835b0e9573cfd5534f866e30cd71359
Yakup Kiliç <32568281+yakup07@users.noreply.github.com>
2022-02-14 14:20:58 +0100

    FetchId

--- src/app/shared/service/edit.service.ts
+++ src/app/shared/service/edit.service.ts
@@ -1,12 +1,36 @@
+import { HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { AnimalProfileComponent } from 'src/app/animal-profile/animal-profile.component';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EditService {
-  editAnimal(newEditAnimalObj: { name: any; weight: any; size: any; chipnumber: any; insurance: any; }) {
-    throw new Error('Method not implemented.');
-  }
+  editAnimal(editAnimal){
+
+    const requestOptions = {
+      method: 'PUT',
+      headers: { 'Content-Type': 'application/json'},
+      body: JSON.stringify(newEditAnimalObj)
+    };
+
+  //   updateAnimal(){
+  //     const httpHeaders = new HttpHeaders();
+  //     const vervangURL = 'http://sonsofkittens.be/api/animals/' + {id}; 
+
+  //   }
+
+  //   getMovieFromId(id: number); Observable<animal]>{
+  //     return of(animal.find => AnimalProfileComponent.id == id));
+  //   }
+
+ }
+
+
 
   constructor() { }
 }
+function newEditAnimalObj(newEditAnimalObj: any) {
+  throw new Error('Function not implemented.');
+}
+
